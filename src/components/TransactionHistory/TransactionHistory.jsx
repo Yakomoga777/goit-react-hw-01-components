fecfa76f62380eb51fcde72a1b5043509a5aa55b
Roman Petrenko <rpetrenko0105@gmail.com>
2023-04-14 18:14:30 +0300

    видаляю зайву деструктуризацію у  TransactionHistory.

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import data from '../../transactions.json';
+// import data from '../../transactions.json';
 import { Table, Tbody, Thead, TRow } from './TransactionHistory.styled';
 
 export function TransactionHistory({ items }) {
-  const { id, type, amount, currency } = items;
+  //   const { id, type, amount, currency } = items;
   return (
     <Table>
       <Thead>
